ada32466a99217a3600c6f03ab4692c150a84f40
Baldomero Cho <don.baldo@hotmail.com>
2023-02-17 23:27:17 -0600

    res

--- lib/widgets/icons/icon_widget.dart
+++ lib/widgets/icons/icon_widget.dart
@@ -11,21 +11,21 @@ class FUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if(width == null && height == null && color == null) {
-      return SvgPicture.asset(file, package: 'fui');
+      return SvgPicture.asset(file, package: 'fui_kit');
     } else if(width == null && height == null) {
-      return SvgPicture.asset(file, color: color, package: 'fui');
+      return SvgPicture.asset(file, color: color, package: 'fui_kit');
     } else if(width == null && color == null) {
-      return SvgPicture.asset(file, height: height, package: 'fui');
+      return SvgPicture.asset(file, height: height, package: 'fui_kit');
     } else if(height == null && color == null) {
-      return SvgPicture.asset(file, width: width, package: 'fui');
+      return SvgPicture.asset(file, width: width, package: 'fui_kit');
     } else if(width == null) {
-      return SvgPicture.asset(file, height: height, color: color, package: 'fui');
+      return SvgPicture.asset(file, height: height, color: color, package: 'fui_kit');
     } else if(height == null) {
-      return SvgPicture.asset(file, width: width, color: color, package: 'fui');
+      return SvgPicture.asset(file, width: width, color: color, package: 'fui_kit');
     } else if(color == null) {
-      return SvgPicture.asset(file, width: width, height: height, package: 'fui');
+      return SvgPicture.asset(file, width: width, height: height, package: 'fui_kit');
     } else {
-      return SvgPicture.asset(file, width: width, height: height, color: color, package: 'fui');
+      return SvgPicture.asset(file, width: width, height: height, color: color, package: 'fui_kit');
     }
   }
 }
